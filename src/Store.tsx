78f9b242797d8dfcbaeccb8c6bda0cc9fabf8511
Kevin Huang <kevin@Kevins-MacBook-Pro.local>
2018-07-12 14:56:30 -0700

    added navbar template

--- src/Store.tsx
+++ src/Store.tsx
@@ -2,33 +2,30 @@ import * as React from 'react';
 
 import './Store.css';
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 
 
 class Store extends React.Component {
   public render() {
     return (
-      <div className="Store">
+      <div className="Store container">
         <header className="Store-header">
           <h1 className="Store-title">Template store</h1>
+          <nav>
+            <div className="navTab container">
+                <ul>
+                  <li><a href="default.asp">Home</a></li>
+                  <li><a href="news.asp">News</a></li>
+                  <li><a href="contact.asp">Contact</a></li>
+                  <li><a href="about.asp">About</a></li>
+                </ul>           
+            </div>
+        
+          </nav>
+          
         </header>
-        <nav className="navbar">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </nav>
-        <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
-
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
+        
+        
       </div>
     );
   }
